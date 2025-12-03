7812869a5ea56e1cdc60440bfba9ee41adfa93d4
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-03 13:23:15 -0600

    Componentes para el menú lateral

---
+++ src/app/gifs/components/side-menu/side-menu.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SideMenuHeaderComponent } from './side-menu-header/side-menu-header';
+import { SideMenuOptionsComponent } from './side-menu-options/side-menu-options';
+
+@Component({
+  selector: 'gifs-side-menu',
+  imports: [
+    SideMenuHeaderComponent,
+    SideMenuOptionsComponent
+  ],
+  templateUrl: './side-menu.html'
+})
+export class SideMenuComponent { }
