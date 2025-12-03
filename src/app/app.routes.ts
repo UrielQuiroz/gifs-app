6766ddc1741e9bed74ae573f384a3f584d004960
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-03 13:05:51 -0600

    rutas hijas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,18 +6,24 @@ export const routes: Routes = [
     path: 'dashboard',
     loadComponent: () =>
       import('./gifs/pages/dashboard-page/dashboard-page'),
-  },
 
-  {
-    path: 'trending',
-    loadComponent: () =>
-      import('./gifs/pages/trending-page/trending-page'),
-  },
+    children: [
+        {
+          path: 'trending',
+          loadComponent: () =>
+            import('./gifs/pages/trending-page/trending-page'),
+        },
 
-  {
-    path: 'search',
-    loadComponent: () =>
-      import('./gifs/pages/search-page/search-page'),
+        {
+          path: 'search',
+          loadComponent: () =>
+            import('./gifs/pages/search-page/search-page'),
+        },
+        {
+          path: '**',
+          redirectTo: 'trending'
+        }
+    ]
   },
 
   {
