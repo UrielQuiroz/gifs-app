ee3d1f03267bfb484d004749826c7256a3ee5eba
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-03 22:32:19 -0600

    Petición HTTP GET

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interface';
+import { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
 
   private http = inject(HttpClient);
+
+  trendingGif = signal<Gif[]>([]);
 
   constructor() {
     this.loadTrendingGifs();
@@ -21,6 +25,11 @@ export class GifService {
         // offser: 0,
         // rating: 'g'
       }
+    })
+    .subscribe(resp => {
+      const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+      this.trendingGif.set(gifs);
+      console.log({ gifs })
     })
 
   }
